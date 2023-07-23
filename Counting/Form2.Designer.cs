c1ea0b6ac5e0b5b5106db4c6bcd04150c114897f
DevipriyaTK <kb.devipriya@gmail.com>
2023-07-23 16:47:35 +0530

    List Class Added

---
+++ Counting/Form2.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Counting
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnFillList = new Button();
+            txtNumberList = new TextBox();
+            cmbDaysList = new ComboBox();
+            gvDays = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)gvDays).BeginInit();
+            SuspendLayout();
+            // 
+            // btnFillList
+            // 
+            btnFillList.Location = new Point(225, 46);
+            btnFillList.Name = "btnFillList";
+            btnFillList.Size = new Size(97, 39);
+            btnFillList.TabIndex = 0;
+            btnFillList.Text = "Fill List";
+            btnFillList.UseVisualStyleBackColor = true;
+            btnFillList.Click += btnFillList_Click;
+            // 
+            // txtNumberList
+            // 
+            txtNumberList.Location = new Point(39, 46);
+            txtNumberList.Multiline = true;
+            txtNumberList.Name = "txtNumberList";
+            txtNumberList.Size = new Size(155, 160);
+            txtNumberList.TabIndex = 1;
+            // 
+            // cmbDaysList
+            // 
+            cmbDaysList.FormattingEnabled = true;
+            cmbDaysList.Location = new Point(225, 125);
+            cmbDaysList.Name = "cmbDaysList";
+            cmbDaysList.Size = new Size(121, 28);
+            cmbDaysList.TabIndex = 2;
+            cmbDaysList.SelectedIndexChanged += cmbDaysList_SelectedIndexChanged;
+            // 
+            // gvDays
+            // 
+            gvDays.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            gvDays.Location = new Point(225, 171);
+            gvDays.Name = "gvDays";
+            gvDays.RowTemplate.Height = 25;
+            gvDays.Size = new Size(328, 189);
+            gvDays.TabIndex = 3;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(10F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(565, 372);
+            Controls.Add(gvDays);
+            Controls.Add(cmbDaysList);
+            Controls.Add(txtNumberList);
+            Controls.Add(btnFillList);
+            Font = new Font("Bookman Old Style", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            Margin = new Padding(4);
+            Name = "Form2";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form2";
+            Load += Form2_Load;
+            ((System.ComponentModel.ISupportInitialize)gvDays).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnFillList;
+        private TextBox txtNumberList;
+        private ComboBox cmbDaysList;
+        private DataGridView gvDays;
+    }
+}
---
+++ Counting/Form2.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Counting
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        List<int> numberList = new List<int>();
+        List<string> textList = new List<string>();
+        List<Employee> employeeList = new List<Employee>();
+
+        public List<Employee> fillList()
+        {
+            employeeList.Clear();
+            Employee employee1 = new Employee();
+            employee1.Id = 1;
+            employee1.Name = "John";
+            employee1.Age = 27;
+            employeeList.Add(employee1);
+            Employee employee2 = new Employee();
+            employee2.Id = 2;
+            employee2.Name = "Sam";
+            employee2.Age = 30;
+            employeeList.Add(employee2);
+            Employee employee3 = new Employee();
+            employee3.Id = 3;
+            employee3.Name = "Charles";
+            employee3.Age = 35;
+            employeeList.Add(employee3);
+            return employeeList;
+
+        }
+
+        List<Days> daysList = new List<Days>();
+
+        private void btnFillList_Click(object sender, EventArgs e)
+        {
+            fillList();
+            foreach (var item in employeeList)
+            {
+                txtNumberList.Text += item.Id + " " + item.Name.ToString() + " " + item.Age + Environment.NewLine;
+            }
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            daysList.Clear();
+            Days day1 = new Days();
+            day1.Id = 1;
+            day1.Day = "Sunday";
+            daysList.Add(day1);
+            Days day2 = new Days();
+            day2.Id = 2;
+            day2.Day = "Monday";
+            daysList.Add(day2);
+            Days day3 = new Days();
+            day3.Id = 3;
+            day3.Day = "Tuesday";
+            daysList.Add(day3);
+            Days day4 = new Days();
+            day4.Id = 4;
+            day4.Day = "Wednesday";
+            daysList.Add(day4);
+            Days day5 = new Days();
+            day5.Id = 5;
+            day5.Day = "Thursday";
+            daysList.Add(day5);
+            Days day6 = new Days();
+            day6.Id = 6;
+            day6.Day = "Friday";
+            daysList.Add(day6);
+            Days day7 = new Days();
+            day7.Id = 7;
+            day7.Day = "Saturday";
+            daysList.Add(day7);
+            cmbDaysList.DataSource = daysList;
+            cmbDaysList.ValueMember = "Id";
+            cmbDaysList.DisplayMember = "Day";
+            gvDays.DataSource = daysList;
+        }
+
+        private void cmbDaysList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //MessageBox.Show(cmbDaysList.SelectedValue.ToString());
+        }
+    }
+}
